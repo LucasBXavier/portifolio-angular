5242ca0a1f285b8a167104cd37da560246dbd86e
LucasBXavier <lucas_b.xavier@hotmail.com>
2025-02-21 15:07:59 -0300

    new cv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { inject } from '@vercel/analytics/*';
+import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 
